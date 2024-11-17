1f8a624d83a7e6c501bd82e1d8e5f9208015affa
Metehan Arıman <metehanariman94@gmail.com>
2024-11-18 00:21:38 +0300

    Newspaper

---
+++ backend/agents/__init__.py
@@ -0,0 +1,10 @@
+from .critique import CritiqueAgent
+from .curator import CuratorAgent
+from .designer import DesignerAgent
+from .editor import EditorAgent
+from .publisher import PublisherAgent
+from .search import SearchAgent
+from .writer import WriterAgent
+
+__all__ = ["CuratorAgent", "DesignerAgent", "SearchAgent", "WriterAgent", "EditorAgent", "PublisherAgent",
+           "CritiqueAgent"]
